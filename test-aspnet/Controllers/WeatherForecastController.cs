c8af7731acc24d55a9d74f7d8c4b93b26a2f77eb
mohamed.gohr <gohr915@gmail.com>
2023-05-25 01:41:40 +0300

    new change

--- test-aspnet/Controllers/WeatherForecastController.cs
+++ test-aspnet/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace test_aspnet.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","colding"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","colding","yellow"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
